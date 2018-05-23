7ec46650a9e512cbdc72f0939c14a31a77cc99bd
Nick <vrm.richards@gmail.com>
2018-05-22 19:46:27 -0600

    worked on fcc stuff

---
+++ JavaScript/format-text.js
@@ -0,0 +1,18 @@
+//how to format and understant JS
+
+//Declarations
+var StUDLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Assignments
+//this is the incorrect way to do things
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+TitleCaseOver = 9000;
+
+// Assignments
+// this is the correct way to write interval
+StUDLyCapVaR = 10;
+properCamelCase = "A String";
+TitleCaseOver = 9000;
---
+++ JavaScript/variables.js
@@ -0,0 +1,19 @@
+/*FCC follow along with lessons*/
+//assigning a variable
+//making a variable
+var myVar = 19;
+
+//making a variable in a different way
+//starting variables that don't have a purpose...yet
+//assigning a variable
+var a = 6;
+//unassigned variables
+var b;
+var c;
+//lines 15-17 are adding things to the existing variable on lines 8-10
+//this is how to add things to existing variables
+a = a + 1;
+b = b + 5;
+c = c + "string!";
+//a way for js to echo/show the code in the console in the browser
+console.log (a,b,c);
