99e288f5d86c4a57c0477c8d6d4b0ac12571c085
Nick <vrm.richards@gmail.com>
2018-05-22 18:50:24 -0600

    change

---
+++ JavaScript/reading-file.js
@@ -0,0 +1,16 @@
+/*FCC follow along with lessons*/
+//assigning a variable
+//making a variable
+var myVar = 19;
+
+//making a variable in a different way
+//starting variables that don't have a purpose...yet
+var a = 6;
+var b;
+var c;
+//assigning variable part
+//adding things to the existing variable
+a = a + 1;
+b = b + 5;
+c = c + "string!";
+console.log (a);
