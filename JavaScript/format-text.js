502dc2c2e79ce048b731443ec1addba5f280ba1b
Nick <vrm.richards@gmail.com>
2018-05-23 23:21:43 -0600

    adding comments

--- JavaScript/format-text.js
+++ JavaScript/format-text.js
@@ -1,6 +1,7 @@
 //how to format and understant JS
 
 //Declarations
+//this is what i want variables to be called
 var StUDLyCapVaR;
 var properCamelCase;
 var TitleCaseOver;
@@ -13,6 +14,7 @@ TitleCaseOver = 9000;
 
 // Assignments
 // this is the correct way to write interval
+//this is what i want to put in the variables
 StUDLyCapVaR = 10;
 properCamelCase = "A String";
 TitleCaseOver = 9000;
